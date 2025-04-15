58feca13a7833e59ee9278c9fa87438a1506076d
andrewhong665 <andrewhong631@gmail.com>
2025-04-15 17:55:00 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -2,12 +2,20 @@ const express = require('express');
 const Gun = require('gun');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const mysql = require('mysql');
 const app = express();
 const port = 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DBNAME,
+  port: process.env.DB_PORT
+});
 
 // Initialize Gun.js
 const gun = Gun({
